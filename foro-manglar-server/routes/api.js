531dc1d00a41dd64a1ad3619fcadc66679dcfe1d
NVD <roman97.46@gmail.com>
2020-11-08 15:54:53 +0100

    implemented registration and login/logout added validation functions for differen types of data added checking functions that automatically send an error response if they arent valid or don't exist and are required added skeleton functions for db crud deleted db connection, used one from imported module changed some statuses to lowercase changed require vars to const added neccesary require consts

--- foro-manglar-server/routes/api.js
+++ foro-manglar-server/routes/api.js
@@ -1,58 +1,61 @@
-var express = require("express");
-var router = express.Router();
-var mongoose = require("mongoose");
-var themeModule = require("../models/theme");
+const express = require("express");
+const router = express.Router();
+const themeModule = require("../models/theme");
+const mongooseConnection = require("../storage/mongoose-connection");
+const passport = require("passport");
+const user = require("../models/user");
 
-//MongoDB connection string
-//mongodb://admin:asdf@localhost:27017/
-let dbUserName = "admin";
-let dbPassword = "asdf";
-let dbLogin = false;
-let dbHost = "localhost";
-let dbPort = "27017";
-let dbConnectionString =
-  "mongodb://" +
-  (dbLogin ? dbUserName + ":" + dbPassword + "@" : "") +
-  dbHost +
-  ":" +
-  dbPort +
-  "/";
-
-//connect to MongoDB
-mongoose.connect(dbConnectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  dbName: "foroManglar",
-});
-
-let db = mongoose.connection;
-//add event handlers
+let db = mongooseConnection.connection;
+//event handlers
 db.on("error", console.error.bind(console, "connection error:"));
 db.on("open", function () {
   console.log("Connected to MongoDB");
 });
 
-//use this to create collections
-/*
-  db.createCollection("objects").catch((e) => {
-    console.log("Collection alredy exists. " + e);
+//ROUTES
+//router.post("/foobar", function (req, res, next) {res.send({ status: "not implemented" });});
+//router.get("/foobar", function (req, res, next) {res.send({ status: "not implemented" });});
+//router.put("/foobar", function (req, res, next) {res.send({ status: "not implemented" });});
+//router.delete("/foobar", function (req, res, next) {res.send({ status: "not implemented" });});
+
+//CREATE
+router.post("/create/theme", function (req, res, next) {
+  let obj = new themeModule();
+
+  //TODO check token
+  let userName = "default"; //TODO get username
+
+  obj.date = Date.now();
+  obj.originalPoster = userName; //TODO check uniqueness
+  obj.title = req.body.title; //TODO check length
+  obj.content = req.body.content; //TODO check length
+
+  console.log(obj); //TODO remove debug log
+
+  try {
+    obj.save();
+  } catch (e) {
+    console.error(e);
+    res.send({
+      status: "error",
+      info: e + "",
+    });
+  }
+
+  res.send({
+    status: "ok",
+    data: obj,
   });
-  */
-
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.render('index', { title: 'API DOCUMENTATION' });
+});
+router.post("/create/post", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.post("/create/comment", function (req, res, next) {
+  res.send({ status: "not implemented" });
 });
 
-//TEST, DELETE
-router.get("/hello", function (req, res, next) {
-  res.contentType("json");
-  let body = { status: "OK", message: "Hello World" };
-  res.send(body);
-});
-
-//TEST, return all items in a collection
-router.get("/get-all/themes", function (req, res, next) {
+//READ
+router.get("/read/themes", function (req, res, next) {
   //res.contentType("json");
 
   async function query(req, res) {
@@ -64,10 +67,7 @@ router.get("/get-all/themes", function (req, res, next) {
     let data = await query.exec();
 
     let body = {
-      status: "OK",
-      info: {
-        dbConnectionString: dbConnectionString,
-      },
+      status: "ok",
       data: data,
     };
 
@@ -77,32 +77,278 @@ router.get("/get-all/themes", function (req, res, next) {
   }
 
   query(req, res).catch(function (error) {
-    let body = { status: "Error", info: error + "" };
+    let body = { status: "error", info: error + "" };
     console.log(error);
     console.log(body);
     res.send(body);
   });
 });
-
-//ADD A THEME
-router.post("/test", function (req, res, next) {
-  let obj = new themeModule();
-
-  //TODO check token and get userName
-  let userName = "default";
-  console.log(Date.now());
-  console.log(req.body.title);
-
-  obj.type = "theme";
-  obj.date = Date.now();
-  obj.originalPoster = userName;
-  obj.title = req.body.title;
-  obj.content = req.body.content;
-
-  console.log(obj);
-  res.send(obj);
-
-  obj.save();
+router.get("/read/themes/:themeId", function (req, res, next) {
+  res.send({ status: "not implemented" });
 });
+router.get("/read/theme/:themeId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.get("/read/posts/:themeId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.get("/read/post/:postId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.get("/read/comments/:postId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.get("/read/comment/:commentId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+
+//UPDATE
+router.put("/update/themes", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.put("/update/themes/:themeId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.put("/update/theme/:themeId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.put("/update/posts/:themeId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.put("/update/post/:postId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.put("/update/comments/:postId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.put("/update/comment/:commentId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+
+//DELETE
+router.delete("/delete/themes", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.delete("/delete/themes/:themeId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.delete("/delete/theme/:themeId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.delete("/delete/posts/:themeId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.delete("/delete/post/:postId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.delete("/delete/comments/:postId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+router.delete("/delete/comment/:commentId", function (req, res, next) {
+  res.send({ status: "not implemented" });
+});
+
+//OTHER
+
+//show API documentation
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "API DOCUMENTATION" });
+});
+
+//test if API is working
+router.get("/hello", function (req, res, next) {
+  res.contentType("json");
+  let body = { status: "ok", message: "Hello World" };
+  res.send(body);
+});
+
+//user management
+router.post("/register", function (req, res, next) {
+  if (
+    checkUsername(req, res, true) ||
+    checkPassword(req, res, true) ||
+    checkEmail(req, res, true) ||
+    checkDescription(req, res)
+  ) {
+    return;
+  }
+
+  let obj = new user({
+    username: req.body.username,
+    enabled: true,
+    userType: "user",
+    email: req.body.email,
+    creationDate: Date.now(),
+    description: req.body.description || "",
+    userConfig: {
+      hideNsfwImages: true,
+      darkTheme: false,
+    },
+  });
+
+  user.register(obj, req.body.password, function (err, user) {
+    if (err) {
+      console.log(JSON.stringify(err, null, 2));
+      return res
+        .status(409)
+        .send({ status: "error", info: err.message, code: 409 });
+    }
+    passport.authenticate("local")(req, res, function () {
+      res.send({ status: "ok" });
+    });
+  });
+});
+
+router.post("/login", function (req, res, next) {
+  if (checkUsername(req, res, true) || checkPassword(req, res, true)) {
+    return;
+  }
+  passport.authenticate("local", function (err, user, info) {
+    if (info) {
+      console.log(info);
+      res.status(401).send(getErrorObject("Wrong credentials", 401));
+    } else {
+      req.login(user, function (err) {
+        if (err) {
+          console.log(err);
+          return res
+            .status(500)
+            .send({ status: "error", info: err.message, code: 500 });
+        }
+        return res.send({ status: "ok" });
+      });
+    }
+  })(req, res, next);
+});
+
+router.post("/logout", function (req, res, next) {
+  req.logout();
+  res.send({
+    status: "ok",
+  });
+});
+
+router.get("/test", function (req, res, next) {
+  res.send({
+    status: "test",
+    info: {
+      user: req.user,
+      username: getUserName(req),
+    },
+  });
+  process.stdout.write("Username: ");
+  console.log(getUserName(req));
+});
+
+//FUNCTIONS
+function getUserName(req) {
+  return req.user && req.user.username;
+}
+function getUserType(req) {
+  return req.user && req.user.userType;
+}
+
+//validators
+function validateUsername(username) {
+  return /^[\-0-9_a-zA-Z]{5,20}$/.test(username);
+}
+function validatePassword(password) {
+  return (
+    password.length >= 12 &&
+    password.length <= 1000 &&
+    /[a-z]/.test(password) &&
+    /[A-Z]/.test(password) &&
+    /[0-9]/.test(password)
+  );
+}
+function validateEmail(email) {
+  return /^[a-zA-Z0-9!#$%&’*+\-/=?^_{|}~](?:[a-zA-Z0-9!#$%&’*+\-/=?^_{|}~.][a-zA-Z0-9!#$%&’*+\-/=?^_{|}~]+)*@[a-zA-Z0-9\[](?:[a-zA-Z0-9-.][a-zA-Z0-9-]+)*\.[a-zA-Z0-9-]*[a-zA-Z0-9\]]$/.test(
+    email
+  );
+}
+function validateDescription(description) {
+  return description.length <= 500;
+}
+function validateContent(content) {
+  return content.length <= 5000;
+}
+
+//checkers: returns true if something doesnt exist or has bad format and sends error object. Otherwise returns false
+function checkUsername(req, res, required) {
+  let obj;
+  if (required && !req.body.username) {
+    obj = getErrorObject("No username was given", 400);
+  } else if (req.body.username && !validateUsername(req.body.username)) {
+    obj = getErrorObject(
+      "Bad username, allowed characters: a-z, A-Z, 0-9, _. Size: 5-20",
+      400
+    );
+  }
+  return sendObject(res, obj);
+}
+function checkPassword(req, res, required) {
+  let obj;
+  if (required && !req.body.password) {
+    obj = getErrorObject("No password was given", 400);
+  } else if (req.body.password && !validatePassword(req.body.password)) {
+    obj = getErrorObject(
+      "Bad password, required at least one of each of these characters: a-z, A-Z, 0-9. Min size: 12",
+      400
+    );
+  }
+  return sendObject(res, obj);
+}
+function checkEmail(req, res, required) {
+  let obj;
+  if (required && !req.body.email) {
+    obj = getErrorObject("No email was given", 400);
+  } else if (req.body.email && !validateEmail(req.body.email)) {
+    obj = getErrorObject(
+      "Bad email format, if your email is functional but isn't accepted use another 'standard' one. Sorry for the inconvenience",
+      400
+    );
+  }
+  return sendObject(res, obj);
+}
+function checkDescription(req, res, required) {
+  let obj;
+  if (required && !res.body.description) {
+    obj = getErrorObject("No description was given", 400);
+  } else if (
+    req.body.description &&
+    !validateDescription(req.body.description)
+  ) {
+    obj = getErrorObject("Bad description, Max size: 500", 400);
+  }
+  return sendObject(res, obj);
+}
+function checkContent(req, res, required) {
+  let obj;
+  if (required && !res.body.content) {
+    obj = getErrorObject("No content was given", 400);
+  } else if (req.body.content && !validateContent(req.body.content)) {
+    obj = getErrorObject("Bad content, Max size: 5000", 400);
+  }
+  return sendObject(res, obj);
+}
+
+//helpers
+
+//if there is an object sends it to res and returns true
+function sendObject(res, obj) {
+  if (obj) {
+    res.status(obj.code || 416).send(obj);
+  }
+  return Boolean(obj);
+}
+
+//returns a simple error object to send to res
+function getErrorObject(message, code) {
+  return {
+    status: "error",
+    message: message,
+    code: code,
+  };
+}
 
 module.exports = router;

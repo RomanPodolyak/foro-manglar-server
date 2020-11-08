44aed50a4fa545a11e62c856a7664fdfdba00cf0
NVD <roman97.46@gmail.com>
2020-11-08 15:44:37 +0100

    added session support and passport init and config added favicon changed var to const in required deleted unnecesary route single quotes in strings changed to double quotes

--- foro-manglar-server/app.js
+++ foro-manglar-server/app.js
@@ -1,24 +1,33 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const favicon = require("serve-favicon");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
-var apiRouter = require('./routes/api');
+const apiRouter = require("./routes/api");
+const mongooseConnection = require("./storage/mongoose-connection");
 
 //creates express aplication
-var app = express();
+const app = express();
 
 //hide express tag in header
-app.disable('x-powered-by');
+app.disable("x-powered-by");
+
+//favicon
+app.use(favicon(__dirname + "/public/favicon/favicon.ico"));
 
 // SETUP MIDDLEWARE
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 // adds request logs to console
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 //parse request and puts it in req.body
 app.use(express.json());
@@ -26,35 +35,52 @@ app.use(express.urlencoded({ extended: false }));
 
 // adds a cookie parser, stored in req.cookies and req.signedCookies
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-// MIDDLEWARE
+//session management
+app.use(
+  session({
+    secret: "cRfUjXn2r5u8x/A?D(G+KbPeSgVkYp3s6v9y$B&E)H@McQfTjWmZq4t7w!z%C*F-",
+    resave: false,
+    saveUninitialized: false,
+    autoRemove: 'interval',
+      autoRemoveInterval: 10, // In minutes
+    store: new MongoStore({
+      mongooseConnection: mongooseConnection.connection,
+    }),
+  })
+);
+
+//passportjs for login management
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport config
+var user = require("./models/user");
+passport.use(new LocalStrategy(user.authenticate()));
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+
+// CUSTOM MIDDLEWARE
 
 // ROUTES
-app.use('/api', apiRouter);
-
-// Fuck that favicon
-app.get('/favicon.ico', (req, res) => res.status(204));
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 // executes if no use or get with valid route caches it first before
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
-});
-
-app.use((err, req, res, next)=>{
-  //res.send("Uh oh...");
+  res.render("error");
 });
 
 module.exports = app;
